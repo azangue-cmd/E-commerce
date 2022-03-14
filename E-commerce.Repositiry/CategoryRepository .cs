5a922af5ae6f5f76d4a9c8e9c9908aa3d4a0b813
ulrich azangue <ulrichazangue@gmail.com>
2022-03-14 05:54:21 +0100

    Repository User and Category done  Fix : #3, #7

---
+++ E-commerce.Repositiry/CategoryRepository .cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Repositiry
+{
+    internal class CategoryRepository
+    {
+        private EcommerceEntities db;
+        public CategoryRepository()
+        {
+            db = new EcommerceEntities();
+        }
+
+        public Category Get (int id)
+        {
+            return db.Categories.FirstOrDefault(X => X.Id == id);
+        }
+
+        public Category Get (string name)
+        {
+            return db.Categories.FirstOrDefault(X => X.Name == name);
+        }
+
+        public Category Add (Category category)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException(nameof(category));
+            }
+
+            var u = Get(category.Name);
+            if (u != null)
+            {
+                throw new DuplicateWaitObjectException($"Category { category.Name } already exist !!!!!!");
+            }
+
+
+            category = db.Categories.Add(category);
+            db.SaveChanges();
+
+            return category;
+        }
+
+        public Category Set (Category category)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException(nameof(category));
+            }
+
+            var oldUser = new EcommerceEntities().Users.Find(category.Id);
+            if (oldUser == null)
+            {
+                throw new KeyNotFoundException($"Category not found !!!!!!");
+            }
+
+
+            var u = Get(category.Name);
+            if (u != null && u.Id != oldUser.Id)
+            {
+                throw new DuplicateWaitObjectException($"Category name { category.Name } already exist !!!!!!");
+            }
+
+
+            //user = db.Users.Add(user);
+            db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return category;
+        }
+
+        public Category Delete (int id)
+        {
+            var category = Get(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+
+            return category;
+        }
+
+        public IEnumerable <Category> Find (Func <Category, bool> predicate)
+        {
+            return db.Categories.Where(predicate).ToArray();
+        }
+
+    }
+}
